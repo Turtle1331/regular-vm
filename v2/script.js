d591d2be1e583b510522ec00c29477387ad8272f
Turtle1331 <turtle1331@users.github.com>
2021-01-27 22:13:41 -0800

    Switch to responsive, thumbnail-style galleries, update tab highlighting

--- v2/script.js
+++ v2/script.js
@@ -8,10 +8,82 @@ document.addEventListener("DOMContentLoaded", () => {
     if (searchForm) {
         searchForm.addEventListener("submit", event => {
             event.preventDefault();
+
             const query = searchForm.elements.searchQuery.value;
             const location = searchLocations[searchForm.elements.searchLocation.value];
             window.location.href = location + encodeURIComponent(query);
+
             return false;
+        });
+    }
+
+    const imageOverlay = document.querySelector("#imageOverlay.overlay");
+    if (imageOverlay) {
+        imageOverlay.addEventListener("click", event => {
+            event.stopPropagation();
+            imageOverlay.innerHTML = "";
+            imageOverlay.style.display = "none";
+            return false;
+        });
+
+        document.querySelectorAll(".image-overlay").forEach(element => {
+            element.addEventListener("click", event => {
+                event.preventDefault();
+                event.stopPropagation();
+
+                // Copy content
+                content = event.target.cloneNode(false);
+                content.classList.add("overlay-content");
+                content.addEventListener("click", event => {
+                    event.stopPropagation();
+                    return false;
+                });
+
+                // Add to overlay
+                imageOverlay.appendChild(content);
+                imageOverlay.style.display = "block";
+
+                return false;
+            }, true);
+        });
+    }
+
+    const videoOverlay = document.querySelector("#videoOverlay.overlay");
+    if (videoOverlay) {
+        videoOverlay.addEventListener("click", event => {
+            event.stopPropagation();
+            videoOverlay.innerHTML = "";
+            videoOverlay.style.display = "none";
+            return false;
+        });
+
+        document.querySelectorAll(".embed-asciicast").forEach(element => {
+            console.log(element);
+            const videoId = element.dataset.asciicastId;
+            `Id: ${videoId}`;
+
+            const preview = document.createElement("img");
+            preview.src = `https://asciinema.org/a/${videoId}.png`;
+            element.appendChild(preview);
+
+            element.addEventListener("click", event => {
+                const content = document.createElement("div");
+                content.classList.add("overlay-content");
+
+                const loading = document.createElement("div");
+                loading.innerText = "Loading...";
+                loading.classList.add("loading-text");
+                content.appendChild(loading);
+
+                const embed = document.createElement("script");
+                embed.src = `https://asciinema.org/a/${videoId}.js`;
+                embed.id = `asciicast-${videoId}`;
+                embed.async = true;
+                content.appendChild(embed);
+
+                videoOverlay.appendChild(content);
+                videoOverlay.style.display = "block";
+            });
         });
     }
 });
