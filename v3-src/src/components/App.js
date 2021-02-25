3572c526bf9e603757965e87344928a0d405de93
Turtle1331 <turtle1331@users.github.com>
2021-02-24 21:07:16 -0800

    Add header, fix style

--- v3-src/src/components/App.js
+++ v3-src/src/components/App.js
@@ -2,16 +2,16 @@ import {useState} from 'react';
 
 import Body from './Body.js';
 import TabList from './TabList.js';
+import Header from './Header.js';
 
 function App() {
   const [tabIndex, setTabIndex] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <TabList tabIndex={tabIndex} setTabIndex={setTabIndex}></TabList>
-        <Body tabIndex={tabIndex}></Body>
-      </div>
+      <Header></Header>
+      <TabList tabIndex={tabIndex} setTabIndex={setTabIndex}></TabList>
+      <Body tabIndex={tabIndex}></Body>
     </div>
   );
 }
