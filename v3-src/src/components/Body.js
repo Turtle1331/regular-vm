c4910743faa50c227ec3f0a3307e29ad1d7e4b89
Turtle1331 <turtle1331@users.github.com>
2021-02-24 21:20:46 -0800

    Add homepage (Table component)

--- v3-src/src/components/Body.js
+++ v3-src/src/components/Body.js
@@ -1,10 +1,11 @@
+import Text from "./Table";
+
 function Body(props) {
     return (
         <div className="Body">
-            <h1>Hello tab {props.tabIndex}!</h1>
             {(() => {
                 switch (props.tabIndex) {
-                    case 0: return <h2>Tab 0</h2>;
+                    case 0: return <Text></Text>;
                     case 1: return <h2>Tab 1</h2>;
                     case 2: return <h2>Tab 2</h2>;
                     case 3: return <h2>Tab 3</h2>;
